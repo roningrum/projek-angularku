c73eb68bc30967cfbbdceaa30cd941868c164b08
roningrum <rizkioktavianingrum@gmail.com>
2023-12-18 14:41:21 +0700

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,29 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'projek-angularku';
+  // menuMakanan =["bakso", "pecel"]
+   menuMakanan =[
+    {
+      namaMakanan :"Bakso",
+      harga : 2000,
+    },
+    {
+      namaMakanan :"Pecel",
+      harga : 5000,
+    },
+    {
+      namaMakanan :"Mie Ayam",
+      harga : 15000,
+    },
+    {
+      namaMakanan :"Ayam Goreng",
+      harga : 10000,
+    },
+   ]
+
+   isRed : boolean = true;
+
+   changeColorList(){
+     this.isRed = !this.isRed;
+   }
 }
